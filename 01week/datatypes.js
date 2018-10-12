0d13ddbeca289171f2e612cd85d0984bb4ef020b
Gabe <jflores353@gmail.com>
2018-10-11 19:33:41 -0500

    modified 01 week datatypes

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -8,48 +8,47 @@ const displayDateAndTime = () =>{
     const time = t.getTime();  
   } 
   // Write a JavaScript program to convert a number to a string.
-  const numberToString = (num) => {
-    const n = num.toString();
+  const numberToString = (num1) => {
+    const n = num1.toString();
   }
   
   // Write a JavaScript program to convert a string to the number.
   const stringToNumber = (x) => {
-    const x = 1;
-    parseInt(x);
+    return parseInt(x);
   }
   
   // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
   const dataType = (arg1) => {
   // Boolean
-    if(typeof arg1 == 'true' || typeof arg1 == 'false'){
+    if(typeof arg1 == true || typeof arg1 == false){
       return 'Boolean'
     }
   // Null
-    else if(typeof arg1 == 'Null' ){
-      return 'Null'
+    else if(typeof arg1 === null ){
+      return null
     }
   // Undefined
-    else if(typeof arg1 == 'Undefined'){
+    else if(typeof arg1 == 'undefined'){
       return 'Undefined'
     }
   // Number
-    else if(typeof arg1 == 'Number'){
+    else if(typeof arg1 == 'number'){
       return 'Number'
     }
   // NaN
-    else if(typeof arg1 != 'Number'){
+    else if(typeof arg1 != 'number'){
       return 'Not a number'
     }
   // String
     else if(typeof arg1 == 'string'){
       return 'String'
-    };
+    }
   }
   // Write a JavaScript program that adds 2 numbers together.
   const addTwoNumbers = (num1, num2) => {return num1 + num2};
   // Write a JavaScript program that runs only when 2 things are true.
   const twoThingsTrue = (arg1, arg2) => {
-    if (arg1 = 'True' && arg2 == 'True'){
+    if (arg1 == true && arg2 == true){
       return 'Both are true!'
     }else{
       return 'One or both are false.';
@@ -57,7 +56,7 @@ const displayDateAndTime = () =>{
   }
   // Write a JavaScript program that runs when 1 of 2 things are true.
   const oneOfTwoTrue = (arg1, arg2) => {
-    if(arg1 = 'True' || arg2 == 'True'){
+    if(arg1 === true || arg2 === true){
     return console.log(oneOfTwoTrue)
   }else{ 
     return 'Neither are true';
