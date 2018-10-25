fbbeb2be5f5ad37bc83bffa75a778650c8dcb6dd
Gabe <jflores353@gmail.com>
2018-10-25 03:01:44 -0500

    first commit to ticTacToe assignment. Still need to complete.

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -1,4 +1,11 @@
 'use strict';
+// Evaluate if Player 1 enters valid 'x' or 'o' in an empty location. Only one play allowed.
+// Update the board. Check for win.
+// Then alert Player 2 for next play.
+// Evaluate if Player 2 enters valid 'x' or 'o' in an empty location.
+// Update the board. Check for win.
+// To check for win evaluate if any array has true values horizontally, verically, or diagonally, else
+// if board has no remaining empty locations then return 'It's a draw'
 
 const assert = require('assert');
 const readline = require('readline');
@@ -23,25 +30,82 @@ function printBoard() {
   console.log('2 ' + board[2].join(' | '));
 }
 
+const player1 = 'X';
+const player2 = 'Y';
+const horizontalWin0 = board[0]
+const horizontalWin1 = board[1]
+const horizontalWin2 = board[2]
+const verticalWin0 = [board[0][0] , board[1][0] , board[2][0]];
+const verticalWin1 = [board[0][1] , board[1][1] , board[2][1]];
+const verticalWin2 = [board[0][2] , board[1][2] , board[2][2]];
+const diagonalWin1 = [board[0][0] , board[1][1] , board[2][2]];
+const diagonalWin2 = [board[0][2] , board[1][1] , board[2][0]];
+
+const ifValidPlay = () =>{
+  let playerTurn = player1;
+  if(player1 !== 'x'){
+    return `Please enter a 'x' to play`;
+  }
+    else{
+      board.splice()
+    }
+    if(player2 !== 'y'){
+      return `Please enter a 'y' to play`;
+    } 
+    else{
+      board.splice();
+    }
+}
+
 function horizontalWin() {
   // Your code here
+  if(player1 == horizontalWin0 || horizontalWin1 || horizontalWin2){
+    return 'Player 1 is the winner with the horizontal set!!!'
+  }else if
+    (player2 == horizontalWin0 || horizontalWin1 || horizontalWin2){
+      return 'Player 2 is the winner with the horizontal set!!!'
+    };
 }
+
 
 function verticalWin() {
   // Your code here
+  if(player1 == verticalWin0 || verticalWin1 || verticalWin2){
+    return `Player 1 is the champ with the vertical win!!!`
+  }else if
+    (player2 == verticalWin0 || verticalWin1 || verticalWin2){
+      return `Player 2 is the champ with the verical win!!!`
+    };
 }
 
 function diagonalWin() {
   // Your code here
+  if(player1 == diagonalWin1 || diagonalWin2){
+    return `Player 1 wins with the diagonal X's!!!`
+  }else if
+    (player2 == diagonalWin1 || diagonalWin2){
+      return `Player 2 wins with the diagonal Y's!!!`
+    };
 }
 
 function checkForWin() {
   // Your code here
+  horizontalWin();
+  verticalWin();
+  diagonalWin();
 }
 
 function ticTacToe(row, column) {
   // Your code here
-}
+    if(player1 == ifValidPlay){
+      checkForWin()
+    } else if
+      (player2 == ifValidPlay){
+        checkForWin()
+      };
+    
+  } 
+
 
 function getPrompt() {
   printBoard();
