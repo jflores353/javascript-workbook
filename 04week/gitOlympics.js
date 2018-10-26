a2e93b461667f1cade39a79b4faa2dab3e204665
Gabe <jflores353@gmail.com>
2018-10-26 14:38:53 -0500

    added to gitOlympics but did not get function to work

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,9 @@
+'use strict';
+const testArray = [test1, test2, test3, test4, test5];
+
+const printListOfOlympians =()=> {
+  printListOfOlympians.forEach((item,index)=>{
+    console.log(item, index)   
+});
+
+printListOfOlympians(item, index);
