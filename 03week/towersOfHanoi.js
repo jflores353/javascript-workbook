839767c9588275b9ed43d4bbea542f418b08bf9c
Gabe <jflores353@gmail.com>
2018-10-30 18:00:31 -0500

    Added to Towers of Hanoi checkpoint but not working properly

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -1,4 +1,9 @@
 'use strict';
+// Goal is to move all disks from stacks.a => stacks.c  (winning stack position)
+// You can only move 1 disk at a time
+// Disks size ranks from smallest to largest 1,2,3,4
+// A larger size disk can NEVER sit above a smaller disk
+// Move disks from A to C by using stack B as an auxiliary 
 
 const assert = require('assert');
 const readline = require('readline');
@@ -13,31 +18,69 @@ let stacks = {
   c: []
 };
 
+const piece1 = 1;
+const piece2 = 2;
+const piece3 = 3;
+const piece4 = 4;
+const winner = [piece1, piece2, piece3, piece4];
+
+let stacksArr = Object.values(stacks);
+let stacksArrLength = stacksArr.length;
+let stacksArrA = stacksArr[0];
+let stacksArrB = stacksArr[1];
+let stacksArrC = stacksArr[2];
+
 function printStacks() {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
-
-function movePiece() {
-  // Your code here
-
+// Only remove one piece at a time from top of any startStack
+// Remove last element only
+function movePiece(startStack, endStack){
+  endStack.push(startStack.pop());
+  console.log('startStack => endStack');
+}
+// Accepts two inputs (towers) one from startStack and one from endStack
+// Will return false if piece from startStack is larger than the piece on the endStack
+// Only movePiece to either an empty 'stacks' or to a 'stacks' with a larger piece already present  
+function isLegal(startStack, endStack) {
+  if(startStack.length == 0){
+    return false;
+  }else if(endStack.length == 0){
+    return true;
+  } 
+  let L1 = startStack.length-1;
+  let L2 = endStack.length-1;
+    if(startStack[L1] < startStack[L2]){
+      return false;
+    } else {
+      return true;
+    }
 }
 
-function isLegal() {
-  // Your code here
-
-}
-
+// All disks must be appropriately stacked at 'stacks.c' for winning position
 function checkForWin() {
-  // Your code here
-
+  if(stacksArrC == winner){
+    return `You have completed the Towers of Hanoi!!!`
+  } 
 }
 
 function towersOfHanoi(startStack, endStack) {
-  // Your code here
-
+  const gameStart = [0, 0, 2, 0, 1, 1];
+  const endStart = [2, 1, 1, 2, 0, 2];
+  //for each element in start array, 
+  for(var i = 0; i < Start.length; i++){
+    if (isLegal(stacksArr[start[i]], stacksArr[end[i]])){
+      movePiece(stacksArr[start[i]], stacksArr[end[i]])
+    } else (isLegal == false);
+      return `Move was not legal from start to end.`
+  }
 }
+// movePiece();
+// isLegal();
+// towersOfHanoi();
+// checkForWin();
 
 function getPrompt() {
   printStacks();
@@ -59,8 +102,25 @@ if (typeof describe === 'function') {
       assert.deepEqual(stacks, { a: [4, 3, 2], b: [1], c: [] });
     });
   });
-
+  // If input is coming from an empty startStack then should return false
   describe('#isLegal()', () => {
+    it('should not allow piece to be removed from an empty stack',()=>{
+      stacks = {
+        a: [4,3,2,1],
+        b: [],
+        c: []
+      };
+      assert.equal(isLegal('b', 'c'), false);
+    });
+    // Aux middle should be allowed to hold all pieces
+    it('should allow all pieces to be fully stacked on aux stack in middle', () => {
+      stacks = {
+        a: [],
+        b: [4,3,2],
+        c: [1]
+      };
+      assert.equal(isLegal('c', 'b'), true);
+    });
     it('should not allow an illegal move', () => {
       stacks = {
         a: [4, 3, 2],
